fb1d683a26c2e020a2e5920cc206ad4c92b4285d
SyntaxError0x7b8 <jtmendez@gmail.com>
2025-08-27 05:52:47 +0100

    added Catch2 test function

--- test/graphics_test.cc
+++ test/graphics_test.cc
@@ -2,4 +2,12 @@
 // Created by qos on 8/26/25.
 //
 
-//#include ""
+#include "catch2/catch_test_macros.hpp"
+#include "../sources/graphics/hero_graphics_component.h"
+//#include  "../cmake-build-debug/_deps/raylib-src/src/raylib.h"
+
+TEST_CASE("HeroScale" "[HeroGraphicsTest]") {
+  auto *hero = new HeroGraphicsComponent(2.3f);
+  CHECK(hero->GetScale() == 2.3f);
+  delete hero;
+}
