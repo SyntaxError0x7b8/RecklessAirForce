417f2edc9ca83916b7f5107c822bf78f6b54d29a
SyntaxError0x7b8 <jtmendez@gmail.com>
2025-09-01 07:28:07 +0100

    updated docstrings

--- src/gameObject/game_object.cc
+++ src/gameObject/game_object.cc
@@ -9,9 +9,10 @@
 #include "game_object.h"
 
 /**
- * @brief Initialise object's position to bottom center of the screen.
- * It assumes a 64px sprite. Screen offset left to personal preference.
+ * @brief Creates a game object. The type of object depends on the components
+ * passed as arguments.
  * @param pgraphics {pointer to GraphicsComponent derived object}
+ * @param pinput {handle user input for player controlled objects}
  */
 GameObject::GameObject(GraphicsComponent *pgraphics, InputHandler *pinput) :
     p_graphics_(pgraphics),
@@ -30,12 +31,21 @@ GameObject::GameObject(GraphicsComponent *pgraphics, InputHandler *pinput) :
   };
 
 }
-
+/**
+ * @brief destroys the game object. The type of game object depends on the
+ * components used for its initialisation. It deletes pointers to components.
+ *
+ */
 GameObject::~GameObject() {
   delete p_graphics_;
-  delete p_input_;
+  delete p_input_; // if enemy, need to delete?
 }
 
+/**
+ * @brief This method calls its components member of the same name.
+ * It will call the component->update() method of each component.
+ * @param kdeltaTime {float}
+ */
 void GameObject::Update(const float kdeltaTime) {
 
   p_input_->Update(*this, kdeltaTime);
@@ -43,13 +53,28 @@ void GameObject::Update(const float kdeltaTime) {
 
 }
 
+/**
+ * @brief Once updated, it draws the object on the screen.
+ */
 void GameObject::Draw() {
   p_graphics_->Draw(*this);
 }
+
+/**
+ * @brief Updates the position of the rectangle necessary for drawing and
+ * collision.
+ * @param x {float, object's x position}
+ * @param y {float, object's y position}
+ */
 void GameObject::UpdateRectangle(const float x, const float y) {
   rect_.x = x;
   rect_.y = y;
 }
+
+/**
+ * @brief Updates the position of the rectangle necessary for drawing and collision.
+ * @param newPosition {Vector2, object's position (x, y)}
+ */
 void GameObject::UpdateRectangle(const Vector2 newPosition) {
   rect_.x = newPosition.x;
   rect_.y = newPosition.y;
